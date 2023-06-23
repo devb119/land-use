6abdea6cfd09d6c9f4f8be98f5bc306fcf1c4a8f
ducanh11092001@gmail.com <ducanh11092001@gmail.com>
2023-06-23 23:00:25 +0700

    remove unused menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import "./App.css";
 import { InfoBox, LegendLayer, Sidebar } from "./components";
-import Menu from "./components/Menu";
 import LeafletMap from "./map/LeafletMap";
 
 function App() {
@@ -8,7 +7,6 @@ function App() {
     <div className="h-screen w-screen relative z-0">
       <LeafletMap />
       <InfoBox />
-      {/* <Menu /> */}
       <Sidebar />
       <LegendLayer />
     </div>
--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
 export { default as SearchBar } from "./SearchBar";
 export { default as MyPopup } from "./MyPopup";
-export { default as Menu } from "./Menu";
 export { default as OWMTileLayer } from "./OWMTileLayer";
 export { default as InfoBox } from "./InfoBox/InfoBox";
 export { default as RoundSpinner } from "./RoundSpinner";
